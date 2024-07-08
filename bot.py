5522a04e2c5775e5ad4a1d98d805d8f74fb87cfa
LP <pobaj69754@breazeim.com>
2024-07-09 01:34:40 +0300

    fixed task command

--- bot.py
+++ bot.py
@@ -13,13 +13,19 @@ from cogs.discord_client import Discord_Client
 load_dotenv()
 secret = os.getenv('BOT_SECRET')
 ban_words = os.getenv('BANNED_WORDS').split(',')
+
 '''
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-        
+    
+    for word in ban_words:
+        if word in message.content:
+            message.delete()
+    
     if message.content.startswith('!test'):
+        
         if message.author.voice != None:
             channel: discord.VoiceChannel = message.author.voice.channel
             connection: discord.VoiceClient = await channel.connect()
@@ -29,20 +35,18 @@ async def on_message(message):
             connection.play(audio, after=lambda e: connection.disconnect()) 
         else:
             await message.channel.send('You should be in the voice channel to use that command')
-    
-    for word in ban_words:
-        if word in message.content:
-            message.delete()
 '''
+
 async def cog_load(client : commands.Bot):
-        for filename in os.listdir("./cogs"):
-            if filename.endswith(".py"):
-                # cut off the .py from the file name
-                await client.load_extension(f"cogs.{filename[:-3]}")
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await client.load_extension(f"cogs.{filename[:-3]}")
 
 async def main():
     client = commands.Bot(command_prefix=commands.when_mentioned_or('!'), intents=discord.Intents.all())
     await cog_load(client)
+    #async with discord.bot:
+    #   await listen_for_twitch_channels.start()
     await client.start(secret)
 
 asyncio.run(main())
--- cogs/discord_client.py
+++ cogs/discord_client.py
@@ -2,9 +2,8 @@ import asyncio
 from datetime import datetime
 import json
 import discord
-from discord.ext import tasks
+from discord.ext import tasks, commands
 import os
-from discord.ext import commands
 from dotenv import load_dotenv
 import youtube_dl
 import urllib.request
@@ -19,10 +18,9 @@ twitch_access = os.getenv('TWITCH_ACCESS_TOKEN')
 ffmpeg_exe_path = os.getenv('FFMPEG_EXE_PATH')
 
 async def send_discord_notification(json_channels,tup,channel):
-
     for channels in json_channels['data']:
         #check if it is not the same stream
-        if channels['id'] == str(tup[0]) and tup[3] != channels['started_at']:
+        if channels['id'] == str(tup[0]) and tup[3] != channels['started_at'] and channel != None:
             insert_stream_start_data(channels['started_at'],tup[0])
             await channel.send(f'@here YOOO {tup[1].upper()} IS LIVE CHECKOUT {tup[2]}')
 
@@ -30,21 +28,45 @@ class Discord_Client(commands.Cog):
 
     def __init__(self, bot):
         super().__init__()
+        sqlite_init()
         self.counter = 0
+        self.listen_for_twitch_channels.start()
         self.bot = bot
 
-    async def setup_hook(self) -> None:
-        self.listen_for_twitch_channels.start()
+    #async def setup_hook(self) -> None:
+     #   print('111')
+     #   await sqlite_init()
+     #   self.loop.create_task(self.isten_for_twitch_channels())
 
     @commands.Cog.listener()
     async def on_ready(self):
         c = datetime.now()
         time = c.strftime('%H:%M:%S')
-        print(f'[ {time} ] Bot started working')
+        print(f'[{time}] Bot started working')
+    
+    @tasks.loop(seconds=10)
+    async def listen_for_twitch_channels(self):
+        try:
+            for tup in get_all_data_tuple():
+                if len(tup) >= 3:
+                    req = urllib.request.Request('https://api.twitch.tv/helix/search/channels?live_only=true&query='+str(tup[1])) 
+                    req.add_header('Authorization', twitch_access)
+                    req.add_header('Client-Id', client_id)
+                    
+                    content = urllib.request.urlopen(req).read()
+                    json_channels = json.loads(content)
+                    ch = self.bot.get_channel(1258405419273949276)
+                    await send_discord_notification(json_channels,tup,ch)
+        except BaseException as e:
+            print('Could not notify about streams')
+            print(e)
 
     @commands.command()
     async def cat_pic(self,ctx):
         urllib.request.urlretrieve('https://cataas.com/cat', 'cats/cat.jpg')
+        c = datetime.now()
+        time = c.strftime('%H:%M:%S')
+        print(f'[{time}] Sending a cat picture')
         await ctx.send(file = discord.File('cats/cat.jpg'))
         await ctx.send.add_reaction('🐱')
 
@@ -53,6 +75,19 @@ class Discord_Client(commands.Cog):
         data = urllib.request.urlopen('https://catfact.ninja/fact').read()
         json_object = json.loads(data.decode('utf-8'))
         await ctx.send(json_object['fact'])
+
+    @commands.command()
+    async def radio(self,ctx):
+        if ctx.author.voice != None:
+            channel: discord.VoiceChannel = ctx.author.voice.channel
+            connection: discord.VoiceClient = await channel.connect()
+            #await urllib.request.urlretrieve('http://radio.garden/api/ara/content/listen/vbFsCngB/channel.mp3','radio.mp3')
+
+            audio = discord.FFmpegPCMAudio(executable=ffmpeg_exe_path,source='mis.mp3')
+            connection.play(audio) 
+            
+        else:
+            await ctx.channel.send('You should be in the voice channel to use that command')
     
     @commands.command()
     async def yt(self,ctx,link):
@@ -68,25 +103,17 @@ class Discord_Client(commands.Cog):
         else:
             await ctx.channel.send('You should be in the voice channel to use that command')
 
-    @tasks.loop(minutes=10)
-    async def listen_for_twitch_channels(self):
-
-        for tup in get_all_data_tuple():
-            if len(tup) >= 3:
-                req = urllib.request.Request('https://api.twitch.tv/helix/search/channels?live_only=true&query='+str(tup[1])) 
-                req.add_header('Authorization', twitch_access)
-                req.add_header('Client-Id', client_id)
-                
-                content = urllib.request.urlopen(req).read()
-                json_channels = json.loads(content)
-                ch = self.get_channel(1258405419273949276)
-                await send_discord_notification(json_channels,tup,ch)
-
-    @listen_for_twitch_channels.before_loop
-    async def before_my_task(self):
-        sqlite_init()
-        await self.wait_until_ready()
+    @radio.error
+    async def info_error(ctx, error):
+        if isinstance(error, commands.BadArgument):
+            await ctx.send('Error?...' + str(error))
         
 async def setup(client: commands.Bot):
     await client.add_cog(Discord_Client(client))
-        
+
+#TO DO this method locks the whole program, in order to make bot leade after playing the source need to figure out how to use that method
+def after_voice_msg_done(channel:discord.VoiceChannel, client:discord.voice_client):
+    coro = channel.send('Song is done!')
+    fut = asyncio.run_coroutine_threadsafe(coro, client.loop)
+    fut.result()
+    client.disconnect()
